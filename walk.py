f62f74536ee129f85e558be4187f3eef647d7b7c
Kite <emailkhyte@gmail.com>
2023-03-08 10:12:04 +0400

    Update walk.py

--- walk.py
+++ walk.py
@@ -1,46 +1,65 @@
-import time, random
+import random
 
-col1 = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
-col2 = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
-col3 = ["O", "O", "O", "O", "O", "X", "O", "O", "O", "O"]
-col4 = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
-col5 = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
+CHARACTER = ""
+while len(CHARACTER) != 1 or CHARACTER == "O":
+    CHARACTER = input("Select Character\n> ")
+
+col1 = [">", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+col2 = [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+col3 = [".", ".", ".", ".", ".", CHARACTER, ".", ".", ".", "."]
+col4 = [".", ".", "O", ".", ".", ".", ".", ".", ".", "."]
+col5 = [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
 
 playerPos = 0   
+def changeBoard():
+    global col1
+    global col2
+    global col3
+    global col4
+    global col5
+    global playerPos
+    col1 = [">", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    col2 = [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    col3 = [".", ".", ".", ".", ".", CHARACTER, ".", ".", ".", "."]
+    col4 = [".", ".", "O", ".", ".", ".", ".", ".", ".", "."]
+    col5 = [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    playerPos = 0
+    showBoard()
+
 def checkPlayerX():
     global playerPos
 
-    if "X" in col1:
+    if CHARACTER in col1:
         for i in col1:
-            if (i == 'X'):
+            if (i == CHARACTER):
                 break
             playerPos += 1
         return "col1"
     
-    if "X" in col2:
+    if CHARACTER in col2:
         for i in col2:
-            if (i == 'X'):
+            if (i == CHARACTER):
                 break
             playerPos += 1
         return "col2"
     
-    if "X" in col3:
+    if CHARACTER in col3:
         for i in col3:
-            if (i == 'X'):
+            if (i == CHARACTER):
                 break
             playerPos += 1
         return "col3"
     
-    if "X" in col4:
+    if CHARACTER in col4:
         for i in col4:
-            if (i == 'X'):
+            if (i == CHARACTER):
                 break
             playerPos += 1
         return "col4"
     
-    if "X" in col5:
+    if CHARACTER in col5:
         for i in col5:
-            if (i == 'X'):
+            if (i == CHARACTER):
                 break
             playerPos += 1
         return "col5"
@@ -75,33 +94,60 @@ def showBoard():
         if posy == "col1":
             showBoard()
         if posy == 'col2':
-            col2[playerPos] = "O"
-            col1[playerPos] = "X"
+            if col1[playerPos] == ">": changeBoard()
+            if col1[playerPos] == "O":
+                playerPos = 0
+                showBoard()
+            col2[playerPos] = "."
+            col1[playerPos] = CHARACTER
         if posy == 'col3':
-            col3[playerPos] = "O"
-            col2[playerPos] = "X"
+            if col2[playerPos] == ">": changeBoard()
+            if col2[playerPos] == "O":
+                col1[playerPos] = "O"
+            col3[playerPos] = "."
+            col2[playerPos] = CHARACTER
         if posy == 'col4':
-            col4[playerPos] = "O"
-            col3[playerPos] = "X"
+            if col3[playerPos] == ">": changeBoard()
+            if col3[playerPos] == "O":
+                col2[playerPos] = "O"
+            col4[playerPos] = "."
+            col3[playerPos] = CHARACTER
         if posy == 'col5':
-            col5[playerPos] = "O"
-            col4[playerPos] = "X"
+            if col4[playerPos] == ">": changeBoard()
+            if col4[playerPos] == "O":
+                col3[playerPos] = "O"
+            col5[playerPos] = "."
+            col4[playerPos] = CHARACTER
+        
         playerPos = 0
 
     if userInput == "s":
         posy = checkPlayerX()
         if posy == "col1":
-            col1[playerPos] = "O"
-            col2[playerPos] = "X"
+            if col2[playerPos] == ">": changeBoard()
+            if col2[playerPos] == "O":
+                col3[playerPos] = "O"
+            col1[playerPos] = "."
+            col2[playerPos] = CHARACTER
         if posy == 'col2':
-            col2[playerPos] = "O"
-            col3[playerPos] = "X"
+            if col3[playerPos] == ">": changeBoard()
+            if col3[playerPos] == "O":
+                col4[playerPos] = "O"
+            col2[playerPos] = "."
+            col3[playerPos] = CHARACTER
         if posy == 'col3':
-            col3[playerPos] = "O"
-            col4[playerPos] = "X"
+            if col4[playerPos] == ">": changeBoard()
+            if col4[playerPos] == "O":
+                col5[playerPos] = "O"
+            col3[playerPos] = "."
+            col4[playerPos] = CHARACTER
         if posy == 'col4':
-            col4[playerPos] = "O"
-            col5[playerPos] = "X"
+            if col5[playerPos] == ">": changeBoard()
+            if col5[playerPos] == "O": 
+                playerPos = 0
+                showBoard()
+            col4[playerPos] = "."
+            col5[playerPos] = CHARACTER
         if posy == 'col5':
             showBoard()
         playerPos = 0
@@ -109,69 +155,102 @@ def showBoard():
     if userInput == "d":
         posx = checkPlayerX()
         if posx == "col1":
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 9:
                 playerPos = 0
                 showBoard()
-            col1[playerPos] = "O"
-            col1[playerPos + 1] = "X"
+            if col1[playerPos + 1] == ">": changeBoard()
+            if col1[playerPos + 1] == "O": col1[playerPos + 2] = "O"
+            col1[playerPos] = "."
+            col1[playerPos + 1] = CHARACTER
         if posx == 'col2':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 9:
                 playerPos = 0
                 showBoard()
-            col2[playerPos] = "O"
-            col2[playerPos + 1] = "X"
+            if col2[playerPos + 1] == ">": changeBoard()
+            if col2[playerPos + 1] == "O": col2[playerPos + 2] = "O"
+            col2[playerPos] = "."
+            col2[playerPos + 1] = CHARACTER
         if posx == 'col3':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 9:
                 playerPos = 0
                 showBoard()
-            col3[playerPos] = "O"
-            col3[playerPos + 1] = "X"
+            if col3[playerPos + 1] == ">": changeBoard()
+            if col3[playerPos + 1] == "O": col3[playerPos + 2] = "O"
+            col3[playerPos] = "."
+            col3[playerPos + 1] = CHARACTER
         if posx == 'col4':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 9:
                 playerPos = 0
                 showBoard()
-            col4[playerPos] = "O"
-            col4[playerPos + 1] = "X"
+            if col4[playerPos + 1] == ">": changeBoard()
+            if col4[playerPos + 1] == "O": col4[playerPos + 2] = "O"
+            col4[playerPos] = "."
+            col4[playerPos + 1] = CHARACTER
         if posx == 'col5':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 9:
                 playerPos = 0
                 showBoard()
-            col5[playerPos] = "O"
-            col5[playerPos + 1] = "X"
+            if col5[playerPos + 1] == "O": col5[playerPos + 2] = "O"
+            col5[playerPos] = "."
+            col5[playerPos + 1] = CHARACTER
     playerPos = 0
 
     if userInput == "a":
         posx = checkPlayerX()
         if posx == "col1":
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 0:
                 playerPos = 0
                 showBoard()
-            col1[playerPos] = "O"
-            col1[playerPos - 1] = "X"
+            if col1[0] == "O":
+                showBoard()
+            if col1[playerPos - 1] == ">": changeBoard()
+            if col1[playerPos - 1] == "O": col1[playerPos - 2] = "O"
+            col1[playerPos] = "."
+            col1[playerPos - 1] = CHARACTER
         if posx == 'col2':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 0:
                 playerPos = 0
                 showBoard()
-            col2[playerPos] = "O"
-            col2[playerPos - 1] = "X"
+            if col2[0] == "O":
+                playerPos = 0
+                showBoard()
+            if col2[playerPos - 1] == ">": changeBoard()
+            if col2[playerPos - 1] == "O": col2[playerPos - 2] = "O"
+            col2[playerPos] = "."
+            col2[playerPos - 1] = CHARACTER
         if posx == 'col3':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 0:
                 playerPos = 0
                 showBoard()
-            col3[playerPos] = "O"
-            col3[playerPos - 1] = "X"
+            if col3[0] == "O":
+                playerPos = 0
+                showBoard()
+            if col3[playerPos - 1] == ">": changeBoard()
+            if col3[playerPos - 1] == "O": col3[playerPos - 2] = "O"
+            col3[playerPos] = "."
+            col3[playerPos - 1] = CHARACTER
         if posx == 'col4':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 0:
                 playerPos = 0
                 showBoard()
-            col4[playerPos] = "O"
-            col4[playerPos - 1] = "X"
+            if col4[0] == "O":
+                playerPos = 0
+                showBoard()
+            if col4[playerPos - 1] == ">": changeBoard()
+            if col4[playerPos - 1] == "O": col4[playerPos - 2] = "O"
+            col4[playerPos] = "."
+            col4[playerPos - 1] = CHARACTER
         if posx == 'col5':
-            if playerPos == 9 or playerPos == 0:
+            if playerPos == 0:
                 playerPos = 0
                 showBoard()
-            col5[playerPos] = "O"
-            col5[playerPos - 1] = "X"
+            if col5[playerPos - 1] == ">": changeBoard()
+            if col5[0] == "O":
+                playerPos = 0
+                showBoard()
+            if col5[playerPos - 1] == "O": col5[playerPos - 2] = "O"
+            col5[playerPos] = "."
+            col5[playerPos - 1] = CHARACTER
     playerPos = 0
 
     showBoard()    
